bc5ea829843a76c48978df1bacdc9efab5da210b
Gogo08 <gogo08190@gmail.com>
2021-12-10 13:07:25 +0100

    Add Leaf, Compost, Plant String items

--- kubejs/startup_scripts/registry/item_registry.js
+++ kubejs/startup_scripts/registry/item_registry.js
@@ -6,4 +6,8 @@ onEvent("item.registry", (event) => {
     });
 
     event.create("chromasky:cobalt_dust").displayName("Cobalt Dust");
+
+    event.create("chromasky:compost").displayName("Compost");
+    event.create("chromasky:leaf").displayName("Leaf");
+    event.create("chromasky:plant_string").displayName("Plant String");
 });
