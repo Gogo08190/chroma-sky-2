78fdc600625125511f7c7d2426a8e892cfad9489
Gogo08 <gogo08190@gmail.com>
2021-12-12 18:58:58 +0100

    Hammer + compressed

--- kubejs/server_scripts/mods/ftbsluice/hammer.js
+++ kubejs/server_scripts/mods/ftbsluice/hammer.js
@@ -9,5 +9,29 @@ onEvent("recipes", (event) => {
     hammer("#forge:netherrack", ["ftbsluice:crushed_netherrack"]);
     hammer("minecraft:basalt", ["ftbsluice:crushed_basalt"]);
     hammer("#forge:end_stones", ["ftbsluice:crushed_endstone"]);
-    
+    hammer("chromasky:1x_compressed_stone", ["9x minecraft:cobblestone"]);
+    hammer("chromasky:2x_compressed_stone", ["18x minecraft:cobblestone"]);
+    hammer("chromasky:3x_compressed_stone", ["27x minecraft:cobblestone"]);
+    hammer("chromasky:1x_compressed_cobblestone", ["9x minecraft:gravel"]);
+    hammer("chromasky:2x_compressed_cobblestone", ["18x minecraft:gravel"]);
+    hammer("chromasky:3x_compressed_cobblestone", ["27x minecraft:gravel"]);
+    hammer("chromasky:1x_compressed_gravel", ["9x minecraft:dirt"]);
+    hammer("chromasky:2x_compressed_gravel", ["18x minecraft:dirt"]);
+    hammer("chromasky:3x_compressed_gravel", ["27x minecraft:dirt"]);
+    hammer("chromasky:1x_compressed_dirt", ["9x minecraft:sand"]);
+    hammer("chromasky:2x_compressed_dirt", ["18x minecraft:sand"]);
+    hammer("chromasky:3x_compressed_dirt", ["27x minecraft:sand"]);
+    hammer("chromasky:1x_compressed_sand", ["9x ftbsluice:dust"]);
+    hammer("chromasky:2x_compressed_sand", ["18x ftbsluice:dust"]);
+    hammer("chromasky:3x_compressed_sand", ["27x ftbsluice:dust"]);
+    hammer("chromasky:1x_compressed_basalt", ["9x ftbsluice:crushed_basalt"]);
+    hammer("chromasky:2x_compressed_basalt", ["18x ftbsluice:crushed_basalt"]);
+    hammer("chromasky:3x_compressed_basalt", ["27x ftbsluice:crushed_basalt"]);
+    hammer("chromasky:1x_compressed_netherrack", ["9x ftbsluice:crushed_netherrack"]);
+    hammer("chromasky:2x_compressed_netherrack", ["18x ftbsluice:crushed_netherrack"]);
+    hammer("chromasky:3x_compressed_netherrack", ["27x ftbsluice:crushed_netherrack"]);
+    hammer("chromasky:1x_compressed_endstone", ["9x ftbsluice:crushed_endstone"]);
+    hammer("chromasky:2x_compressed_endstone", ["18x ftbsluice:crushed_endstone"]);
+    hammer("chromasky:3x_compressed_endstone", ["27x ftbsluice:crushed_endstone"]);
+
 });
--- kubejs/server_scripts/mods/utils.js
+++ kubejs/server_scripts/mods/utils.js
@@ -11,6 +11,38 @@ onEvent("recipes", (event) => {
 
     event.shapeless('minecraft:oak_leaves', '9x chromasky:leaf');
 
+    event.shapeless('chromasky:1x_compressed_stone', '9x minecraft:stone');
+    event.shapeless('chromasky:2x_compressed_stone', '9x chromasky:1x_compressed_stone');
+    event.shapeless('chromasky:3x_compressed_stone', '9x chromasky:2x_compressed_stone');
+
+    event.shapeless('chromasky:1x_compressed_cobblestone', '9x minecraft:cobblestone');
+    event.shapeless('chromasky:2x_compressed_cobblestone', '9x chromasky:1x_compressed_cobblestone');
+    event.shapeless('chromasky:3x_compressed_cobblestone', '9x chromasky:2x_compressed_cobblestone');
+
+    event.shapeless('chromasky:1x_compressed_gravel', '9x minecraft:gravel');
+    event.shapeless('chromasky:2x_compressed_gravel', '9x chromasky:1x_compressed_gravel');
+    event.shapeless('chromasky:3x_compressed_gravel', '9x chromasky:2x_compressed_gravel');
+
+    event.shapeless('chromasky:1x_compressed_dirt', '9x minecraft:dirt');
+    event.shapeless('chromasky:2x_compressed_dirt', '9x chromasky:1x_compressed_dirt');
+    event.shapeless('chromasky:3x_compressed_dirt', '9x chromasky:2x_compressed_dirt');
+
+    event.shapeless('chromasky:1x_compressed_sand', '9x minecraft:sand');
+    event.shapeless('chromasky:2x_compressed_sand', '9x chromasky:1x_compressed_sand');
+    event.shapeless('chromasky:3x_compressed_sand', '9x chromasky:2x_compressed_sand');
+
+    event.shapeless('chromasky:1x_compressed_basalt', '9x minecraft:basalt');
+    event.shapeless('chromasky:2x_compressed_basalt', '9x chromasky:1x_compressed_basalt');
+    event.shapeless('chromasky:3x_compressed_basalt', '9x chromasky:2x_compressed_basalt');
+
+    event.shapeless('chromasky:1x_compressed_endstone', '9x minecraft:end_stone');
+    event.shapeless('chromasky:2x_compressed_endstone', '9x chromasky:1x_compressed_endstone');
+    event.shapeless('chromasky:3x_compressed_endstone', '9x chromasky:2x_compressed_endstone');
+
+    event.shapeless('chromasky:1x_compressed_netherrack', '9x minecraft:netherrack');
+    event.shapeless('chromasky:2x_compressed_netherrack', '9x chromasky:1x_compressed_netherrack');
+    event.shapeless('chromasky:3x_compressed_netherrack', '9x chromasky:2x_compressed_netherrack');
+
     event.shaped('minecraft:dirt', [
     'CC ',
     'CC '
