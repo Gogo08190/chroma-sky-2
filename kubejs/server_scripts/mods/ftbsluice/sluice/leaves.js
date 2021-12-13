e74d9194482d018cbde239afef36f46ec2841af3
koepss <pierrelode2@outlook.fr>
2021-12-13 18:42:02 +0100

    sluice cloth leave add results

--- kubejs/server_scripts/mods/ftbsluice/sluice/leaves.js
+++ kubejs/server_scripts/mods/ftbsluice/sluice/leaves.js
@@ -2,8 +2,14 @@ onEvent("recipes", (event) => {
     const sluice = event.recipes.ftbsluice.sluice;
 
     sluice(["cloth"], "#minecraft:leaves", [
-        ["minecraft:flint", 0.17],
-        ["minecraft:coal", 0.14],
+        ["pamhc2trees:durian_sapling", 0.17],
+        ["pamhc2trees:walnut_sapling", 0.17],
+        ["pamhc2trees:spiderweb_sapling", 0.17],
+        ["pamhc2trees:olive_sapling", 0.17],
+        ["pamhc2trees:lime_sapling", 0.17],
+        ["pamhc2trees:paperbark_sapling", 0.17],
+        ["pamhc2trees:cinnamon_sapling", 0.17],
+        ["pamhc2trees:passionfruit_sapling", 0.17],
     ]).max(2); //.time().mb();
 
     sluice(["iron"], "#minecraft:leaves", [
