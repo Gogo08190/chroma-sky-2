e72e4098aece68f92148d8148420ae8befcf1b94
Gogo08 <gogo08190@gmail.com>
2021-12-15 20:52:46 +0100

    Endstone sluice

--- kubejs/server_scripts/mods/ftbsluice/sluice/endstone.js
+++ kubejs/server_scripts/mods/ftbsluice/sluice/endstone.js
@@ -10,12 +10,18 @@ onEvent("recipes", (event) => {
         ["thermal:ender_pearl_dust", 0.30],
         ["mysticalagriculture:prosperity_shard", 0.30],
         ["tconstruct:manyullyn_nugget", 0.05],
+        ["minecraft:chorus_flower", 0.05],
+        ["miniutilities:ender_lily_seeds", 0.06],
     ]);
 
     sluice(["diamond"], "ftbsluice:crushed_endstone", [
         ["mysticalagriculture:prosperity_shard", 0.35],
         ["thermal:ender_pearl_dust", 0.35],
         ["tconstruct:manyullyn_nugget", 0.09],
+        ["draconicevolution:draconium_dust", 0.05],
+        ["minecraft:shulker_shell", 0.02],
+        ["minecraft:chorus_flower", 0.12],
+        ["miniutilities:ender_lily_seeds", 0.12],
     ]);
 
     sluice(["blazing"], "ftbsluice:crushed_endstone", [
