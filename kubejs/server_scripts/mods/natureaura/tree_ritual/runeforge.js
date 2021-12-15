f91a70ca6aad03f886349866f599f58b832e6800
mathieu <mathieucrombe@gmail.com>
2021-12-15 18:44:51 +0100

    change bug runeforge

--- kubejs/server_scripts/mods/natureaura/tree_ritual/runeforge.js
+++ kubejs/server_scripts/mods/natureaura/tree_ritual/runeforge.js
@@ -1,19 +1,20 @@
 onEvent('recipes', event => {
     event.remove({ output: 'mana-and-artifice:runeforge' })
+
     event.custom({
         "type": "naturesaura:tree_ritual",
         "ingredients": [
             {
-                "item": "#mana-and-artifice:stone_runes"
+                "tag": "#mana-and-artifice:stone_runes"
             },
             {
-                "item": "#mana-and-artifice:stone_runes"
+                "tag": "#mana-and-artifice:stone_runes"
             },
             {
-                "item": "#mana-and-artifice:stone_runes"
+                "tag": "#mana-and-artifice:stone_runes"
             },
             {
-                "item": "#mana-and-artifice:stone_runes"
+                "tag": "#mana-and-artifice:stone_runes"
             },
             {
                 "item": "naturesaura:infused_stone"
