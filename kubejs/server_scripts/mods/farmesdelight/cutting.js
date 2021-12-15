bdc3cf03a2eab8cdcb39bc19e46d191d722fa5e5
Gogo08 <41335079+Gogo08190@users.noreply.github.com>
2021-12-15 15:11:57 +0100

    Update cutting.js

--- kubejs/server_scripts/mods/farmesdelight/cutting.js
+++ kubejs/server_scripts/mods/farmesdelight/cutting.js
@@ -16,9 +16,27 @@ events.listen('recipes', function (event) {
     })
   }
 
+  // Cutting Board Shovel Cutting
+  const shovelCutting = (output, input) => {
+    event.custom({
+      type: "farmersdelight:cutting",
+      ingredients: [
+        input
+      ],
+      tool: {
+        "tag": "forge:tools/shovel"
+      },
+      result: output
+    })
+  }
+
   // ----- Knife Recipes -----
 
   knifeCutting([Item.of('4x chromasky:leaf')], Ingredient.of('#minecraft:leaves'))
   knifeCutting([Item.of('chromasky:plant_string')], Ingredient.of('chromasky:leaf'))
+
+  // -----Shovel Recipes -----
+
+  shovelCutting([Item.of('chromasky:flint_shard')], Ingredient.of('minecraft:flint'))
 
 })
