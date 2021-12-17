a048a53ee3977fd03a1665d52cb6d66787a9ca29
Gogo08 <gogo08190@gmail.com>
2021-12-17 07:41:22 +0100

    Unify part one

--- kubejs/server_scripts/unify.js
+++ kubejs/server_scripts/unify.js
@@ -11,4 +11,74 @@ onEvent("recipes", (event) => {
         .id("ftbjarmod:cast_iron_gear");
 
     event.replaceInput({ output: "#forge:gears" }, "minecraft:iron_nugget", "#forge:nuggets/cast_iron");
+
+
+    const { smelting, blasting } = event.recipes.minecraft;
+
+    var unifyMetal = function (name, hasOre, nuggetItem, ingotItem, blockItem, dustItem, gearItem, plateItem) {
+        if (ingotItem !== "") event.replaceOutput("#forge:ingots/" + name, ingotItem);
+        if (dustItem !== "") event.replaceOutput("#forge:dusts/" + name, dustItem);
+        if (nuggetItem !== "") event.replaceOutput("#forge:nuggets/" + name, nuggetItem);
+        if (blockItem !== "") event.replaceOutput("#forge:storage_blocks/" + name, blockItem);
+        if (gearItem !== "") event.replaceOutput("#forge:gears/" + name, gearItem);
+        if (plateItem !== "") event.replaceOutput("#forge:plates/" + name, plateItem);
+        event.remove({ output: "#forge:ingots/" + name, type: "minecraft:smelting" });
+        event.remove({ output: "#forge:ingots/" + name, type: "minecraft:blasting" });
+        if (dustItem !== "" && ingotItem !== "") {
+            smelting(ingotItem, "#forge:dusts/" + name).xp(0.7);
+            blasting(ingotItem, "#forge:dusts/" + name).xp(0.7);
+        }
+        if (hasOre && ingotItem !== "") {
+            const oresStrict = Ingredient.of("#forge:ores/" + name).filter(
+                Ingredient.of("#forge:clusters/" + name).not()
+            );
+            smelting(ingotItem, oresStrict).xp(0.7);
+            blasting(ingotItem, oresStrict).xp(0.7);
+        }
+    };
+
+    unifyMetal(
+        "iron",
+        true,
+        "minecraft:iron_nugget",
+        "minecraft:iron_ingot",
+        "minecraft:iron_block",
+        "thermal:iron_dust",
+        "thermal:iron_gear",
+        "thermal:iron_plate"
+    );
+
+    unifyMetal(
+        "gold",
+        true,
+        "minecraft:gold_nugget",
+        "minecraft:gold_ingot",
+        "minecraft:gold_block",
+        "thermal:gold_dust",
+        "thermal:gold_gear",
+        "thermal:gold_plate"
+    );
+
+    unifyMetal(
+        "diamond",
+        true,
+        "",
+        "",
+        "minecraft:diamond_block",
+        "thermal:diamond_dust",
+        "thermal:diamond_gear",
+         ""
+   );
+
+   unifyMetal(
+        "copper",
+        true,
+        "thermal:copper_nugget",
+        "thermal:copper_ingot",
+        "thermal:copper_block",
+        "thermal:copper_dust",
+        "thermal:copper_gear",
+        "thermal:copper_plate"
+    );
+
 });
