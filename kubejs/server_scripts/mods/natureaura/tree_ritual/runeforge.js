8587a15fa8d86b3f9588baa5c8851493a7856b07
koepss <pierrelode2@outlook.fr>
2021-12-17 18:34:24 +0100

    add garden cloche tree ritual

--- kubejs/server_scripts/mods/natureaura/tree_ritual/runeforge.js
+++ kubejs/server_scripts/mods/natureaura/tree_ritual/runeforge.js
@@ -39,5 +39,44 @@ onEvent('recipes', event => {
         "time": 200
     })
 
+    event.custom({
+        "type": "naturesaura:tree_ritual",
+        "ingredients": [
+            {
+                "item": "immersiveengineering:electric_lantern"
+            },
+            {
+                "item": "immersiveengineering:component_steel"
+            },
+            {
+                "item": "immersiveengineering:fluid_pump"
+            },
+            {
+                "item": "immersiveengineering:component_steel"
+            },
+            {
+                "item": "minecraft:glass"
+            },
+            {
+                "item": "immersiveengineering:treated_wood_horizontal"
+            },
+            {
+                "item": "immersiveengineering:treated_wood_horizontal"
+            },
+            {
+                "item": "minecraft:glass"
+            }
+        ],
+        "sapling": {
+            "item": "minecraft:acacia_sapling"
+        },
+        "output": {
+            "item": "immersiveengineering:cloche",
+            "count": 1
+        },
+        "time": 200
+    })
+
+
 
 });
