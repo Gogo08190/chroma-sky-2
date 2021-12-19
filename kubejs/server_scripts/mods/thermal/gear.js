d4a2d7c32e3c7488fd8c9e1704946677675a1094
Gogo08 <gogo08190@gmail.com>
2021-12-19 10:26:33 +0100

    Thermal

---
+++ kubejs/server_scripts/mods/thermal/gear.js
@@ -0,0 +1,11 @@
+onEvent("recipes", (event) => {
+  event.recipes.thermal.press("thermal:lapis_gear", ["4x minecraft:lapis_lazuli", "thermal:press_gear_die"]);
+  event.recipes.thermal.press("thermal:diamond_gear", ["4x minecraft:diamond", "thermal:press_gear_die"]);
+  event.recipes.thermal.press("thermal:emerald_gear", ["4x minecraft:emerald", "thermal:press_gear_die"]);
+  event.recipes.thermal.press("thermal:quartz_gear", ["4x minecraft:quartz", "thermal:press_gear_die"]);
+  event.recipes.thermal.press("ftbjarmod:cast_iron_gear", ["4x ftbjarmod:cast_iron_ingot", "thermal:press_gear_die"])
+  event.recipes.thermal.press("thermal:sapphire_gear", ["4x thermal:sapphire", "thermal:press_gear_die"])
+  event.recipes.thermal.press("thermal:ruby_gear", ["4x thermal:ruby", "thermal:press_gear_die"])
+  event.recipes.thermal.press("chromasky:redstone_gear", ["4x extendedcrafting:redstone_ingot", "thermal:press_gear_die"])
+  event.recipes.thermal.press("chromasky:treated_wood_gear", ["4x immersiveengineering:treated_wood_horizontal", "thermal:press_gear_die"])
+});
