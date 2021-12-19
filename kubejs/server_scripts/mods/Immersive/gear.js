14634ed19fc81887aaad9c188e5ee94460b76b86
Gogo08 <gogo08190@gmail.com>
2021-12-19 10:27:08 +0100

    Immersive recipe

---
+++ kubejs/server_scripts/mods/Immersive/gear.js
@@ -0,0 +1,68 @@
+onEvent("recipes", (event) => {
+event.recipes.immersiveengineering.metal_press(
+        "thermal:lapis_gear",
+        "4x minecraft:lapis_lazuli",
+        "immersiveengineering:mold_gear"
+    );
+event.recipes.immersiveengineering.metal_press(
+    "thermal:diamond_gear",
+    "4x minecraft:diamond",
+    "immersiveengineering:mold_gear"
+);
+
+event.recipes.immersiveengineering.metal_press(
+    "thermal:emerald_gear",
+    "4x minecraft:emerald",
+    "immersiveengineering:mold_gear"
+);
+
+event.recipes.immersiveengineering.metal_press(
+    "thermal:quartz_gear",
+    "4x minecraft:quartz",
+    "immersiveengineering:mold_gear"
+);
+
+event.recipes.immersiveengineering.metal_press(
+    "ftbjarmod:cast_iron_gear",
+    "4x ftbjarmod:cast_iron_ingot",
+    "immersiveengineering:mold_gear"
+);
+
+event.recipes.immersiveengineering.metal_press(
+    "thermal:signalum_gear",
+    "4x thermal:signalum_ingot",
+    "immersiveengineering:mold_gear"
+);
+
+event.recipes.immersiveengineering.metal_press(
+    "thermal:lumium_gear",
+    "4x thermal:lumium_ingot",
+    "immersiveengineering:mold_gear"
+);
+
+event.recipes.immersiveengineering.metal_press(
+    "thermal:enderium_gear",
+    "4x thermal:enderium_ingot",
+    "immersiveengineering:mold_gear"
+);
+event.recipes.immersiveengineering.metal_press(
+    "chromasky:redstone_gear",
+    "4x extendedcrafting:redstone_ingot",
+    "immersiveengineering:mold_gear"
+);
+event.recipes.immersiveengineering.metal_press(
+    "thermal:sapphire_gear",
+    "4x thermal:sapphire",
+    "immersiveengineering:mold_gear"
+);
+event.recipes.immersiveengineering.metal_press(
+    "thermal:ruby_gear",
+    "4x thermal:ruby",
+    "immersiveengineering:mold_gear"
+);
+event.recipes.immersiveengineering.metal_press(
+    "chromasky:treated_wood_gear",
+    "4x immersiveengineering:treated_wood_horizontal",
+    "immersiveengineering:mold_gear"
+);
+});
---
+++ kubejs/server_scripts/mods/Immersive/machine.js
@@ -0,0 +1,3 @@
+onEvent("recipes", (event) => {
+  event.recipes.immersiveengineeringAlloy('extendedcrafting:redstone_ingot', 'ftbjarmod:cast_iron_ingot', 'projectred-core:red_ingot')
+});
