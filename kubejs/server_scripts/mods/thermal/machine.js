6841c66edbf1df28331ea46aa8ba85edced99b03
Gogo08 <gogo08190@gmail.com>
2021-12-20 13:23:33 +0100

    Secondary output

--- kubejs/server_scripts/mods/thermal/machine.js
+++ kubejs/server_scripts/mods/thermal/machine.js
@@ -11,5 +11,5 @@ onEvent("recipes", (event) => {
     .custom({"type": "thermal:refinery","ingredient": {"fluid": "thermal:latex","amount": 100},"result": [{"item": "thermal:rubber","chance": 1}],"energy": 10000})
 
   event
-    .custom({"type": "thermal:pyrolyzer","ingredient": {"item": "thermal:rubber"},"result": [{"item": "thermal:cured_rubber"},{"item": 'industrialforegoing:dryrubber',"chance": 0.05}],"energy": 5000,"experience": 0.00})
+    .custom({"type": "thermal:pyrolyzer","ingredient": {"item": "thermal:rubber"},"result": [{"item": "thermal:cured_rubber"},{"item": 'thermal:cured_rubber',"chance": 0.5},{"item": 'industrialforegoing:dryrubber',"chance": 0.05}],"energy": 5000,"experience": 0.00})
 });
