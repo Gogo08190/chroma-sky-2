e0072aeaf1a3a54c585bc1965c34d591f965b493
Gogo08 <gogo08190@gmail.com>
2021-12-20 07:36:48 +0100

    Redstone ingot

--- kubejs/server_scripts/mods/thermal/machine.js
+++ kubejs/server_scripts/mods/thermal/machine.js
@@ -2,4 +2,8 @@ onEvent("recipes", (event) => {
   event.recipes.thermal
     .smelter("thermal:machine_frame", ["chromasky:top_frame", "chromasky:mechanic_core_tier_1", "chromasky:bottom_frame"])
     .energy(5000);
+
+  event.recipes.thermal
+    .smelter("extendedcrafting:redstone_ingot", ["ftbjarmod:cast_iron_ingot", "projectred-core:red_ingot"])
+    .energy(2000);
 });
