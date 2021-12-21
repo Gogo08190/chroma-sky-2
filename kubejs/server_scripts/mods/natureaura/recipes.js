e2eb85320ee2ebe22505cb927a931dbaed27cc37
koepss <pierrelode2@outlook.fr>
2021-12-22 00:20:32 +0100

    add craft Gold fiber and Golden stone brick (nature's aura)

---
+++ kubejs/server_scripts/mods/natureaura/recipes.js
@@ -0,0 +1,28 @@
+onEvent('recipes', event => {
+
+  // Gold Fiber
+  event.remove({ output: 'naturesaura:gold_fiber' })
+  event.shaped('4x naturesaura:gold_fiber', [
+  'LRL',
+  'GTG',
+  'LRL'
+  ], {
+    L: '#minecraft:leaves',
+    R: 'tconstruct:rose_gold_nugget',
+    G: 'minecraft:gold_nugget',
+    T: 'quark:turf'
+  })
+
+  // Golden Stone Brick
+  event.remove({ output: 'naturesaura:gold_brick' })
+  event.shaped('naturesaura:gold_brick', [
+  'GSG',
+  'SGS',
+  'GSG'
+  ], {
+    G: 'naturesaura:gold_fiber',
+    S: 'minecraft:stone_bricks'
+
+  })
+
+    });
