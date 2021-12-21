f205c3815ebb7b80301bbe10e8c402f77cc664d3
Gogo08 <gogo08190@gmail.com>
2021-12-21 18:42:10 +0100

    RFTools machine frames recipe

---
+++ kubejs/server_scripts/mods/rftools/recipes.js
@@ -0,0 +1,23 @@
+onEvent("recipes", (event) => {
+  event.remove({ output: 'rftoolsbase:machine_base' })
+  event.shaped('rftoolsbase:machine_base', [
+  'PGP',
+  'SSS'
+  ], {
+    S: 'thermal:polished_slag',
+    G: 'thermal:gold_gear',
+    P: 'thermal:gold_plate'
+  })
+
+  event.remove({ output: 'rftoolsbase:machine_frame' })
+  event.shaped('rftoolsbase:machine_frame', [
+  'SGS',
+  'PCP',
+  'SGS'
+  ], {
+    S: 'mekanism:ingot_steel',
+    G: 'thermal:lapis_gear',
+    P: 'thermal:gold_plate',
+    C: 'chromasky:mechanic_core_tier_2'
+  })
+});
