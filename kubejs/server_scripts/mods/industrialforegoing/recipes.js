668e631be18753131df804f89a695b5453bcb264
Gogo08 <gogo08190@gmail.com>
2021-12-22 12:58:58 +0100

    Ore laser base recipe

--- kubejs/server_scripts/mods/industrialforegoing/recipes.js
+++ kubejs/server_scripts/mods/industrialforegoing/recipes.js
@@ -1,5 +1,4 @@
 onEvent('recipes', event => {
-
   event.remove({ output: 'industrialforegoing:machine_frame_pity' })
   event.shaped('industrialforegoing:machine_frame_pity', [
   'TRT',
@@ -9,5 +8,19 @@ onEvent('recipes', event => {
     C: 'chromasky:mechanic_core_tier_2',
     S: 'mekanism:ingot_steel',
     T: 'immersiveengineering:treated_wood_horizontal'
+  })
+
+  event.remove({ output: 'industrialforegoing:ore_laser_base' })
+  event.shaped('industrialforegoing:ore_laser_base', [
+  'POP',
+  'DFD',
+  'GRG'
+  ], {
+    P: 'industrialforegoing:plastic',
+    O: 'occultism:infused_pickaxe',
+    F: 'industrialforegoing:machine_frame_advanced',
+    D: '#forge:ores/diamond',
+    G: 'thermal:enderium_gear',
+    R: 'extendedcrafting:redstone_ingot'
   })
 });
