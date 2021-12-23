5e1b84f5636b574ec20afbdd6daf5f1328d94240
Gogo08 <gogo08190@gmail.com>
2021-12-23 17:14:10 +0100

    Add custom

--- kubejs/server_scripts/mods/Powah/recipes.js
+++ kubejs/server_scripts/mods/Powah/recipes.js
@@ -37,4 +37,42 @@ onEvent('recipes', event => {
     R: 'thermal:rf_coil',
   })
 
+  event.custom(
+    {
+        "input": [
+            {
+                "item": "mekanism:ingot_steel"
+            },
+            {
+                "item": "industrialforegoing:pink_slime_ingot"
+            },
+            {
+                "item": "mekanism:ingot_steel"
+            },
+            {
+                "item": "rftoolsbase:dimensionalshard"
+            },
+            {
+                "item": "rftoolsbase:dimensionalshard"
+            },
+            {
+                "item": "mekanism:ingot_steel"
+            },
+            {
+                "item": "industrialforegoing:pink_slime_ingot"
+            },
+            {
+                "item": "mekanism:ingot_steel"
+            }
+        ],
+        "inputFluid": "{FluidName:\"industrialforegoing:ether_gas\",Amount:1000}",
+        "processingTime": 320,
+        "output": {
+            "item": "chromasky:mechanic_core_tier_3",
+            "count": 1
+        },
+        "type": "industrialforegoing:dissolution_chamber"
+    }
+  )
+
 });
