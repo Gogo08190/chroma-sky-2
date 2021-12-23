8e891794bfff6ae4893b8122c232f07427e3d3f0
Gogo08 <gogo08190@gmail.com>
2021-12-23 07:42:48 +0100

    Machine casing Refined

--- kubejs/server_scripts/mods/refined/recipes.js
+++ kubejs/server_scripts/mods/refined/recipes.js
@@ -6,4 +6,15 @@ onEvent('recipes', event => {
   event.recipes.thermal
     .smelter("4x refinedstorage:quartz_enriched_iron", ['3x minecraft:iron_ingot', 'minecraft:quartz'])
     .energy(6400);
+
+  event.remove({ output: 'refinedstorage:machine_casing' })
+  event.shaped('refinedstorage:machine_casing', [
+  'QSQ',
+  'SCS',
+  'QSQ'
+  ], {
+    Q: 'refinedstorage:quartz_enriched_iron',
+    S: 'immersiveengineering:component_steel',
+    C: 'chromasky:mechanic_core_tier_3'
+  })
 });
