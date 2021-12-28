d86603b4a8698cb4114a01b0d9213db344ace39d
koepss <pierrelode2@outlook.fr>
2021-12-28 22:23:06 +0100

    add craft black iron ingot

--- kubejs/server_scripts/mods/extended/recipes.js
+++ kubejs/server_scripts/mods/extended/recipes.js
@@ -2,4 +2,10 @@ onEvent("recipes", (event) => {
   event.remove({ output: 'extendedcrafting:redstone_ingot' })
   event.shapeless('extendedcrafting:redstone_ingot', '9x extendedcrafting:redstone_nugget')
   event.shaped('9x extendedcrafting:redstone_ingot', 'extendedcrafting:redstone_ingot_block')
+
+  event.remove({ output: 'extendedcrafting:black_iron_ingot' })
+
+  event.recipes.thermal
+    .smelter("extendedcrafting:black_iron_ingot", ['immersiveengineering:coal_coke', 'mekanism:ingot_steel'])
+    .energy(6400);
 });
