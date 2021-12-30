a9253d3235faf9cd9257d6b88939357f2ffb6478
mathieucrombe <mathieucrombe@gmail.com>
2021-12-30 23:23:05 +0100

    add recipe for ender essence

---
+++ kubejs/server_scripts/mods/manaandartefice/manaweaving.js
@@ -0,0 +1,19 @@
+onEvent('recipes', event => {
+
+    event.custom({
+        "type": "mana-and-artifice:manaweaving-recipe",
+        "output": "chromasky:enderessence",
+        "tier": 1,
+        "items": [
+            "minecraft:ender_eye",
+            "minecraft:ender_pearl",
+            "ironfurnaces:obsidian_furnace",
+            "minecraft:nether_star"
+        ],
+        "patterns": [
+            "mana-and-artifice:circle",
+            "mana-and-artifice:triangle",
+            "mana-and-artifice:circle"
+        ]
+    })
+})
