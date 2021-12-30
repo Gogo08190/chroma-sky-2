94f88053e2a77ee49423512139745a3d386bc49e
koepss <pierrelode2@outlook.fr>
2021-12-30 17:44:35 +0100

    add craft

---
+++ kubejs/server_scripts/mods/Astralsorcery/recipes.js
@@ -0,0 +1,11 @@
+onEvent('recipes', event => {
+
+  // Collector Crystal
+  event.shaped(Item.of('astralsorcery:rock_collector_crystal', '{astralsorcery:{constellation:"astralsorcery:evorsio",crystalProperties:{attributes:[{discovered:1b,property:"astralsorcery:size",pLevel:1},{discovered:1b,property:"astralsorcery:purity",pLevel:1},{discovered:1b,property:"astralsorcery:shape",pLevel:1},{discovered:1b,property:"astralsorcery:collector.rate",pLevel:1},{discovered:1b,property:"astralsorcery:constellation.evorsio",pLevel:1}]}}}'), [
+  'AAA',
+  'ABA',
+  'AAA'
+  ], {
+    B: 'minecraft:bone_meal',
+    A: 'astralsorcery:aquamarine'
+  })
