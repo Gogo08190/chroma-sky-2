897e30fb03ac77cb2aeb25c1fe27a8cd10d24824
koepss <pierrelode2@outlook.fr>
2022-01-03 17:35:46 +0100

    add villager

---
+++ kubejs/server_scripts/mods/natureaura/animalspawner.js
@@ -0,0 +1,21 @@
+onEvent('recipes', event => {
+
+  event.custom({
+    {
+        "type": "naturesaura:animal_spawner",
+        "ingredients": [
+            {
+                "item": "naturesaura:birth_spirit"
+            },
+            {
+                "item": "minecraft:egg"
+            },
+            {
+                "item": "minecraft:anvil"
+            }
+        ],
+        "entity": "minecraft:villager",
+        "aura": 50000,
+        "time": 60
+    })
+});
