efa05de3a5da1ebf4f5f6a91070e3bcbfcf206e8
Gogo08 <gogo08190@gmail.com>
2022-01-04 14:31:34 +0100

    Mekasuit

--- kubejs/server_scripts/mods/mekanism/recipes.js
+++ kubejs/server_scripts/mods/mekanism/recipes.js
@@ -84,4 +84,56 @@ onEvent("recipes", (event) => {
     R: 'mekanism:alloy_reinforced',
     O: 'mekanism:ingot_osmium'
   })
+
+  event.remove({ output: 'mekanism:mekasuit_helmet' })
+  event.shaped('mekanism:mekasuit_helmet', [
+  'HCH',
+  'HAH',
+  'PBP'
+  ], {
+    H: 'mekanism:hdpe_sheet',
+    C: 'mekanism:ultimate_control_circuit',
+    P: 'mekanism:pellet_polonium',
+    B: 'mekanism:basic_induction_cell',
+    A: 'theabyss:unorithe_a_netherithe_helmet'
+  })
+
+  event.remove({ output: 'mekanism:mekasuit_bodyarmor' })
+  event.shaped('mekanism:mekasuit_bodyarmor', [
+  'HCH',
+  'HAH',
+  'PBP'
+  ], {
+    H: 'mekanism:hdpe_sheet',
+    C: 'mekanism:ultimate_control_circuit',
+    P: 'mekanism:pellet_polonium',
+    B: 'mekanism:basic_induction_cell',
+    A: 'theabyss:unorithe_a_netherithe_chestplate'
+  })
+
+  event.remove({ output: 'mekanism:mekasuit_pants' })
+  event.shaped('mekanism:mekasuit_pants', [
+  'HCH',
+  'HAH',
+  'PBP'
+  ], {
+    H: 'mekanism:hdpe_sheet',
+    C: 'mekanism:ultimate_control_circuit',
+    P: 'mekanism:pellet_polonium',
+    B: 'mekanism:basic_induction_cell',
+    A: 'theabyss:unorithe_a_netherithe_leggings'
+  })
+
+  event.remove({ output: 'mekanism:mekasuit_boots' })
+  event.shaped('mekanism:mekasuit_boots', [
+  'HCH',
+  'HAH',
+  'PBP'
+  ], {
+    H: 'mekanism:hdpe_sheet',
+    C: 'mekanism:ultimate_control_circuit',
+    P: 'mekanism:pellet_polonium',
+    B: 'mekanism:basic_induction_cell',
+    A: 'theabyss:unorithe_a_netherithe_boots'
+  })
 });
