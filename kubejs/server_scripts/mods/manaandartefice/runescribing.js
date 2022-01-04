85523112a1b3e63ee977213077a5718c807c18b3
mathieu <mathieucrombe@gmail.com>
2022-01-04 21:30:23 +0100

    add runescribing craft

---
+++ kubejs/server_scripts/mods/manaandartefice/runescribing.js
@@ -0,0 +1,10 @@
+onEvent('recipes', event => {
+
+    event.custom({
+        "type": "mana-and-artifice:runescribing",
+        "tier": 2,
+        "mutex_h": 28737120478717464,
+        "mutex_v": 30417173679837292,
+        "output": "minecraft:dirt"
+    })
+})
