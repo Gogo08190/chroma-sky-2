0b8b259b358b6fb5768b2f5db03698ca58aeb505
mathieu <mathieucrombe@gmail.com>
2022-01-04 21:31:32 +0100

    change tier for craft of the piece of star

--- kubejs/server_scripts/mods/manaandartefice/runescribing.js
+++ kubejs/server_scripts/mods/manaandartefice/runescribing.js
@@ -2,7 +2,7 @@ onEvent('recipes', event => {
 
     event.custom({
         "type": "mana-and-artifice:runescribing",
-        "tier": 2,
+        "tier": 1,
         "mutex_h": 28737120478717464,
         "mutex_v": 30417173679837292,
         "output": "minecraft:dirt"
