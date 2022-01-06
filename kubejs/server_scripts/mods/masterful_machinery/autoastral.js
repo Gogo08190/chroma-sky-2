869e24cca87eed86c192258469b1a285e329d483
koepss <pierrelode2@outlook.fr>
2022-01-06 21:03:29 +0100

    add craft autoastral

--- kubejs/server_scripts/mods/masterful_machinery/autoastral.js
+++ kubejs/server_scripts/mods/masterful_machinery/autoastral.js
@@ -174,11 +174,14 @@ e.custom({
 
   // Astral 2 Items
   astral2('astralsorcery:aquamarine', 2, 'minecraft:glowstone_dust', 2, 'astralsorcery:illumination_powder', 16, 100)
-
+  astral2('minecraft:glass_pane', 1, 'astralsorcery:aquamarine', 2, 'astralsorcery:glass_lens', 1, 50)
+  astral2('astralsorcery:marble_raw', 4, 'minecraft:charcoal', 1, 'astralsorcery:black_marble_raw', 8, 40)
+  astral2('minecraft:paper', 2, 'astralsorcery:aquamarine', 1, 'astralsorcery:parchment', 1, 60)
+  
   // Astral 3 Items
   astral3(input1, count1, input2, count2, input3, count3, output, countout, starlight)
-  
+
   // Astral 4 Items
-  astral4(input1, count1, input2, count2, input3, count3, input4, count4, output, countout, starlight)
+  astral4('minecraft:oak_planks', 1, 'astralsorcery:marble_raw', 1, 'minecraft:gold_nugget', 1, 'astralsorcery:glass_lens', 1, 'astralsorcery:spectral_relay', 2, 70)
 
 })
