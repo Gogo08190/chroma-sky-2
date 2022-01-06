5d7d3456a178a37782732a4e70691c8e4cbc6127
Gogo08 <gogo08190@gmail.com>
2022-01-06 18:29:25 +0100

    Auto astral

---
+++ kubejs/server_scripts/mods/masterful_machinery/autoastral.js
@@ -0,0 +1,127 @@
+events.listen('recipes', function (e) {
+e.custom({
+  "type": "masterfulmachinery:machine_structure",
+  "id": "auto_astral",
+  "controllerId": "autoastral",
+  "name": "Auto Iridescent Altar",
+  "layout": [
+    [
+      ")))     )))",
+      ")*+)))))+*)",
+      ")+-------+)",
+      " )-**-**-) ",
+      " )-*****-) ",
+      " .--***--/ ",
+      " )-*****-) ",
+      " )-**-**-) ",
+      ")+-------+)",
+      ")*+))C))+*)",
+      ")))     )))"
+    ],
+    [
+      "000     000",
+      "01 00000 10",
+      "0         0",
+      " 0       0 ",
+      " 0       0 ",
+      " 0   2   0 ",
+      " 0       0 ",
+      " 0       0 ",
+      "0         0",
+      "01 00000 10",
+      "000     000"
+    ],
+    [
+      "           ",
+      " 3       3 ",
+      "           ",
+      "           ",
+      "           ",
+      "           ",
+      "           ",
+      "           ",
+      "           ",
+      " 3       3 ",
+      "           "
+    ],
+    [
+      "           ",
+      " 3       3 ",
+      "           ",
+      "           ",
+      "           ",
+      "           ",
+      "           ",
+      "           ",
+      "           ",
+      " 3       3 ",
+      "           "
+    ],
+    [
+      "           ",
+      " 4)     )4 ",
+      " )       ) ",
+      "           ",
+      "           ",
+      "           ",
+      "           ",
+      "           ",
+      " )       ) ",
+      " 4)     )4 ",
+      "           "
+    ],
+    [
+      "           ",
+      "           ",
+      "  ))) )))  ",
+      "  )     )  ",
+      "  )     )  ",
+      "     5     ",
+      "  )     )  ",
+      "  )     )  ",
+      "  ))) )))  ",
+      "           ",
+      "           "
+    ]
+  ],
+  "legend": {
+    ")": {
+      "block": "astralsorcery:black_marble_bricks"
+    },
+    "*": {
+      "block": "astralsorcery:black_marble_raw"
+    },
+    "+": {
+      "block": "simplylight:illuminant_black_block_on"
+    },
+    "-": {
+      "block": "astralsorcery:marble_raw"
+    },
+    ".": {
+      "block": "masterfulmachinery:autoastral_autoastral_item_port_items_output"
+    },
+    "/": {
+      "block": "masterfulmachinery:autoastral_autoastral_item_port_items_input"
+    },
+    "0": {
+      "block": "astralsorcery:infused_wood_slab"
+    },
+    "1": {
+      "block": "astralsorcery:black_marble_runed"
+    },
+    "2": {
+      "block": "astralsorcery:altar_radiance"
+    },
+    "3": {
+      "block": "astralsorcery:marble_pillar"
+    },
+    "4": {
+      "block": "astralsorcery:black_marble_chiseled"
+    },
+    "5": {
+      "block": "masterfulmachinery:autoastral_autoastral_starlight_port_astral_starlight_input"
+    }
+  }
+})
+
+})
