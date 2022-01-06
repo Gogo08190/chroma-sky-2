1f064202c8e96b72c8bf07bbad5672e73a90d2f1
Gogo08 <gogo08190@gmail.com>
2022-01-06 13:19:01 +0100

    Remove recipes

--- kubejs/server_scripts/mods/industrialforegoing/recipes.js
+++ kubejs/server_scripts/mods/industrialforegoing/recipes.js
@@ -1,14 +1,5 @@
 onEvent('recipes', event => {
   event.remove({ output: 'industrialforegoing:machine_frame_pity' })
-  event.shaped('industrialforegoing:machine_frame_pity', [
-  'TST',
-  'SCS',
-  'TST'
-  ], {
-    C: 'chromasky:mechanic_core_tier_2',
-    S: 'mekanism:ingot_steel',
-    T: 'immersiveengineering:treated_wood_horizontal'
-  })
 
   event.remove({ output: 'industrialforegoing:ore_laser_base' })
   event.shaped('industrialforegoing:ore_laser_base', [
