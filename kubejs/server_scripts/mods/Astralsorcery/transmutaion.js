a066cc28deb4a121886ddacb0933c917c929e3cd
koepss <pierrelode2@outlook.fr>
2022-01-07 14:55:01 +0100

    fix

--- kubejs/server_scripts/mods/Astralsorcery/transmutaion.js
+++ kubejs/server_scripts/mods/Astralsorcery/transmutaion.js
@@ -3,10 +3,13 @@ onEvent('recipes', event => {
   event.custom(
     {
       "type": "astralsorcery:block_transmutation",
-      "starlight": 100,
       "input": [
         {
-          "item": "astralsorcery:starmetal"
+          "block": "astralsorcery:starmetal",
+          "display": {
+            "item": "astralsorcery:starmetal",
+            "count": 1
+          }
         }
       ],
       "output": {
@@ -15,7 +18,10 @@ onEvent('recipes', event => {
       "display": {
         "item": "masterfulmachinery:autoastral_autoastral_starlight_port_astral_starlight_input",
         "count": 1
-      }
+      },
+      "starlight": 60
     }
-  )
-  });
+  }
+)
+
+ });
