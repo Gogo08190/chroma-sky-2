a2d7bd57454b873057d949ed216961660a8c6fa4
Gogo08 <gogo08190@gmail.com>
2022-01-09 11:13:34 +0100

    Fix autoastral recipes

--- kubejs/server_scripts/mods/masterful_machinery/autoastral.js
+++ kubejs/server_scripts/mods/masterful_machinery/autoastral.js
@@ -159,8 +159,8 @@ e.custom({
   // Astral 2 Items
   astral2('astralsorcery:aquamarine', 1, 'minecraft:glowstone_dust', 4, 'astralsorcery:illumination_powder', 16, 100)
   astral2('minecraft:glass_pane', 1, 'astralsorcery:aquamarine', 4, 'astralsorcery:glass_lens', 1, 50)
-  astral2('astralsorcery:marble_raw', 4, 'minecraft:coal', 1, 'astralsorcery:black_marble_raw', 8, 40)
-  astral2('minecraft:paper', 2, 'astralsorcery:aquamarine', 1, 'astralsorcery:parchment', 1, 60)
+  astral2('astralsorcery:marble_raw', 8, 'minecraft:coal', 1, 'astralsorcery:black_marble_raw', 8, 40)
+  astral2('minecraft:paper', 4, 'astralsorcery:aquamarine', 1, 'astralsorcery:parchment', 1, 60)
 
   // Astral 4 Items
   astral4('minecraft:oak_planks', 2, 'astralsorcery:marble_raw', 1, 'minecraft:gold_nugget', 2, 'astralsorcery:glass_lens', 1, 'astralsorcery:spectral_relay', 2, 70)
