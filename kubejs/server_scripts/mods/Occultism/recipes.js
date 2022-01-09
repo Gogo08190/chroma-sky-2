e171faa754c97786c7fb052d17b6b0db28e6cbff
Victor <lerouxvictor3@gmail.com>
2022-01-09 15:47:09 +0100

    Edited craft of the Prudentium Crystal in recipes.js
    Deleted ritual.js

--- kubejs/server_scripts/mods/Occultism/recipes.js
+++ kubejs/server_scripts/mods/Occultism/recipes.js
@@ -106,4 +106,33 @@ onEvent("recipes", (event) => {
         T: 'occultism:otherstone_tablet',
         B: 'quark:iron_button'
     })
+
+    //Edited craft of Prudentium Crystal
+
+    onEvent('recipes', (event) => {
+
+        event.remove({ output: 'matc:prudentium:crystal' });
+
+        event.custom(
+            {
+                type: 'occultism:ritual',
+                activation_item: 'matc:inferium:crystal',
+                pentacle_id: 'occultism:craft_foliot',
+                duration: 240,
+                ritual_dummy: 'matc:prudentium:crystal',
+                ingredients: [
+                    { item: 'mysticalagriculture:prudentium_gemstone' },
+                    { item: 'mysticalagriculture:prudentium_essence' },
+                    { item: 'mysticalagriculture:prudentium_gemstone' },
+                    { item: 'mysticalagriculture:prosperity_shard' },
+                    { item: 'mysticalagriculture:prosperity_shard' },
+                    { item: 'mysticalagriculture:prudentium_gemstone' },
+                    { item: 'mysticalagriculture:prudentium_essence' },
+                    { item: 'mysticalagriculture:prudentium_gemstone' },
+                ],
+                result: { item: 'matc:prudentium:crystal' },
+                id: 'occultism/ritual/prudentium_crystal'
+            }
+        )
+    })
 });
