49f5e0a37e05d7aa1a145009f8f434aaefd56a3d
Gogo08 <gogo08190@gmail.com>
2022-01-09 15:43:51 +0100

    indent

--- kubejs/server_scripts/mods/Occultism/ritual.js
+++ kubejs/server_scripts/mods/Occultism/ritual.js
@@ -1,26 +1,26 @@
 onEvent('recipes', (event) => {
 
-    event.remove({ output: 'matc:prudentium:crystal' });
+  event.remove({ output: 'matc:prudentium:crystal' });
 
-    event.custom(
-        {
-            type: 'occultism:ritual',
-            activation_item: 'matc:inferium:crystal',
-            pentacle_id: 'occultism:craft_foliot',
-            duration: 240,
-            ritual_dummy: 'matc:prudentium:crystal',
-            ingredients: [
-                { item: 'mysticalagriculture:prudentium_gemstone' },
-                { item: 'mysticalagriculture:prudentium_essence' },
-                { item: 'mysticalagriculture:prudentium_gemstone' },
-                { item: 'mysticalagriculture:prosperity_shard' },
-                { item: 'mysticalagriculture:prosperity_shard' },
-                { item: 'mysticalagriculture:prudentium_gemstone' },
-                { item: 'mysticalagriculture:prudentium_essence' },
-                { item: 'mysticalagriculture:prudentium_gemstone' },
-            ],
-            result: { item: 'matc:prudentium:crystal' },
-            id: 'occultism/ritual/prudentium_crystal'
-        }
-    )
+  event.custom(
+    {
+        type: 'occultism:ritual',
+        activation_item: 'matc:inferium:crystal',
+        pentacle_id: 'occultism:craft_foliot',
+        duration: 240,
+        ritual_dummy: 'matc:prudentium:crystal',
+        ingredients: [
+            { item: 'mysticalagriculture:prudentium_gemstone' },
+            { item: 'mysticalagriculture:prudentium_essence' },
+            { item: 'mysticalagriculture:prudentium_gemstone' },
+            { item: 'mysticalagriculture:prosperity_shard' },
+            { item: 'mysticalagriculture:prosperity_shard' },
+            { item: 'mysticalagriculture:prudentium_gemstone' },
+            { item: 'mysticalagriculture:prudentium_essence' },
+            { item: 'mysticalagriculture:prudentium_gemstone' },
+        ],
+        result: { item: 'matc:prudentium:crystal' },
+        id: 'occultism/ritual/prudentium_crystal'
+    }
+  )
 })
