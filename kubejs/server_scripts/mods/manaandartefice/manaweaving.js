bcac3de2f6c50eca9f8f906770c5424609d0ed91
Gogo08 <gogo08190@gmail.com>
2022-01-11 09:17:23 +0100

    Tertium crystal

--- kubejs/server_scripts/mods/manaandartefice/manaweaving.js
+++ kubejs/server_scripts/mods/manaandartefice/manaweaving.js
@@ -1,19 +1,42 @@
 onEvent('recipes', event => {
 
-    event.custom({
-        "type": "mana-and-artifice:manaweaving-recipe",
-        "output": "chromasky:enderessence",
-        "tier": 1,
-        "items": [
-            "minecraft:ender_eye",
-            "minecraft:ender_pearl",
-            "minecraft:obsidian",
-            "minecraft:nether_star"
-        ],
-        "patterns": [
-            "mana-and-artifice:circle",
-            "mana-and-artifice:triangle",
-            "mana-and-artifice:circle"
-        ]
-    })
+  event.custom({
+    "type": "mana-and-artifice:manaweaving-recipe",
+    "output": "chromasky:enderessence",
+    "tier": 1,
+    "items": [
+        "minecraft:ender_eye",
+        "minecraft:ender_pearl",
+        "minecraft:obsidian",
+        "minecraft:nether_star"
+    ],
+    "patterns": [
+        "mana-and-artifice:circle",
+        "mana-and-artifice:triangle",
+        "mana-and-artifice:circle"
+    ]
+  })
+
+  event.remove({ output: 'matc:tertium_crystal' })
+  event.custom({
+    "type": "mana-and-artifice:manaweaving-recipe",
+    "output": "matc:tertium_crystal",
+    "tier": 2,
+    "items": [
+        "mysticalagriculture:tertium_gemstone",
+        "mysticalagriculture:tertium_gemstone",
+        "mysticalagriculture:tertium_gemstone",
+        "mysticalagriculture:prosperity_ingot",
+        "matc:prudentium_crystal",
+        "mysticalagriculture:prosperity_ingot",
+        "mysticalagriculture:tertium_essence",
+        "mysticalagriculture:tertium_essence",
+        "mysticalagriculture:tertium_essence"
+    ],
+    "patterns": [
+        "mana-and-artifice:hourglass",
+        "mana-and-artifice:infinity",
+        "mana-and-artifice:split_triangle"
+    ]
+  })
 })
