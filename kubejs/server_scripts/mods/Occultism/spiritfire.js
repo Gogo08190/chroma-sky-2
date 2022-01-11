8c03d110b1dfba61614cf5747b4c5a96b06074ca
mathieu <mathieucrombe@gmail.com>
2022-01-11 21:08:28 +0100

    add craft for magical infusse book

---
+++ kubejs/server_scripts/mods/Occultism/spiritfire.js
@@ -0,0 +1,15 @@
+onEvent('recipes', event => {
+
+    event.custom({
+        
+            "type": "occultism:spirit_fire",
+            "ingredient": {
+                "item": "occultism:book_of_binding_foliot"
+            },
+            "result": {
+                "item": "chromasky:magical_infuse_book"
+            }
+        
+    })
+
+})
