69cf3c3a9710e33a92facae2827fc2460728029d
Gogo08 <gogo08190@gmail.com>
2022-01-15 16:16:27 +0100

    Add emoji

---
+++ kubejs/server_scripts/mods/emojiful.js
@@ -0,0 +1,17 @@
+events.listen("recipes", (event) => {
+    event.recipes.emojiful.emoji_recipe({
+        category: "Chroma",
+        name: "chroma_technology_2",
+        url: "https://cdn.discordapp.com/emojis/856211906863562762.png",
+    });
+    event.recipes.emojiful.emoji_recipe({
+        category: "Chroma",
+        name: "chroma_sky_1",
+        url: "https://cdn.discordapp.com/emojis/805753968223059968.png",
+    });
+    event.recipes.emojiful.emoji_recipe({
+        category: "Chroma",
+        name: "chroma_technology_1",
+        url: "https://cdn.discordapp.com/emojis/737343564098568323.png",
+    });
+});
