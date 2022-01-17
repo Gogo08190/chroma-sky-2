a40bf278bfcb128d8add0cc92104792d33c1bc95
Gogo08 <gogo08190@gmail.com>
2022-01-17 18:43:12 +0100

    Crystaltine recipe

--- kubejs/server_scripts/mods/extended/recipes.js
+++ kubejs/server_scripts/mods/extended/recipes.js
@@ -14,14 +14,18 @@ event.remove({ output: 'miniutilities:angel_ring' })
     .smelter("extendedcrafting:black_iron_ingot", ['immersiveengineering:coal_coke', 'mekanism:ingot_steel', 'rats:little_black_squash_balls'])
     .energy(6400);
 
-    event.shaped('9x extendedcrafting:ender_ingot', 'extendedcrafting:ender_ingot_block')
-    event.shapeless('extendedcrafting:ender_ingot', '9x extendedcrafting:ender_nugget')
+  event.shaped('9x extendedcrafting:ender_ingot', 'extendedcrafting:ender_ingot_block')
+  event.shapeless('extendedcrafting:ender_ingot', '9x extendedcrafting:ender_nugget')
 
-    event.remove({ output: 'extendedcrafting:ender_ingot' })
-    event.shaped('2x extendedcrafting:ender_ingot', [
-    'SI'
-    ], {
-      S: 'powah:steel_energized',
-      I: 'rftoolsbase:infused_enderpearl'
-    })
+  event.remove({ output: 'extendedcrafting:ender_ingot' })
+  event.shaped('2x extendedcrafting:ender_ingot', [
+  'SI'
+  ], {
+    S: 'powah:steel_energized',
+    I: 'rftoolsbase:infused_enderpearl'
+  })
+
+  event.remove({ output: 'extendedcrafting:crystaltine_ingot' })
+  event.shapeless('extendedcrafting:crystaltine_ingot', '9x extendedcrafting:crystaltine_nugget');
+  event.shapeless('9x extendedcrafting:crystaltine_ingot', 'extendedcrafting:crystaltine_block');
 });
