a958de9274e5fb8827a3fa928a50865261c7bfaf
Gogo08 <gogo08190@gmail.com>
2022-01-19 18:48:05 +0100

    Fix sluice

--- kubejs/server_scripts/mods/ftbsluice/sluice/dust.js
+++ kubejs/server_scripts/mods/ftbsluice/sluice/dust.js
@@ -2,53 +2,54 @@ onEvent("recipes", (event) => {
     const sluice = event.recipes.ftbsluice.sluice;
 
     sluice(["cloth"], "ftbsluice:dust", [
-      ["minecraft:flint", 0.48],
-      ["projectred-core:electrotine_dust", 0.23],
-      ["chromasky:iron_chunk", 0.30],
-      ["chromasky:coal_chunk", 0.21],
-    ]).mb(50);
+        ["chromasky:gold_chunk", 0.04],
+        ["minecraft:bone_meal", 0.11],
+        ["chromasky:silver_chunk", 0.05],
+        ["chromasky:nickel_chunk", 0.04],
+    ]).max(2);
 
     sluice(["iron"], "ftbsluice:dust", [
-        ["minecraft:flint", 0.22],
-        ["chromasky:lapis_chunk", 0.21],
-        ["chromasky:redstone_chunk", 0.19],
-        ["thermal:ruby", 0.15],
-        ["thermal:sapphire", 0.14],
         ["projectred-core:peridot", 0.16],
+        ["chromasky:gold_chunk", 0.07],
+        ["minecraft:bone_meal", 0.16],
+        ["minecraft:gunpowder", 0.09],
+        ["chromasky:fluorite_chunk", 0.15],
+        ["chromasky:silver_chunk", 0.09],
+        ["chromasky:nickel_chunk", 0.07],
     ]);
 
     sluice(["gold"], "ftbsluice:dust", [
-        ["chromasky:osmium_chunk", 0.15],
-        ["chromasky:fluorite_chunk", 0.15],
         ["projectred-core:peridot", 0.2],
-        ["thermal:sapphire", 0.2],
-        ["chromasky:lapis_chunk", 0.28],
-        ["chromasky:redstone_chunk", 0.21],
-        ["thermal:ruby", 0.2],
         ["mana-and-artifice:vinteum_dust", 0.05],
+        ["chromasky:fluorite_chunk", 0.2],
+        ["chromasky:gold_chunk", 0.11],
+        ["minecraft:bone_meal", 0.21],
+        ["minecraft:gunpowder", 0.14],
+        ["chromasky:redstone_chunk", 0.21],
+        ["chromasky:silver_chunk", 0.13],
+        ["chromasky:nickel_chunk", 0.11],
     ]);
 
     sluice(["diamond"], "ftbsluice:dust", [
         ["mana-and-artifice:vinteum_dust", 0.1],
-        ["chromasky:iron_chunk", 0.30],
         ["chromasky:redstone_chunk", 0.3],
-        ["chromasky:lapis_chunk", 0.33],
-        ["chromasky:osmium_chunk", 0.2],
         ["chromasky:fluorite_chunk", 0.21],
-        ["thermal:sapphire", 0.24],
         ["projectred-core:peridot", 0.23],
-        ["thermal:ruby", 0.22],
+        ["chromasky:gold_chunk", 0.16],
+        ["minecraft:bone_meal", 0.25],
+        ["minecraft:gunpowder", 0.19],
+        ["chromasky:redstone_chunk", 0.21],
+        ["chromasky:silver_chunk", 0.15],
+        ["chromasky:nickel_chunk", 0.14],
     ]);
 
     sluice(["blazing"], "ftbsluice:dust", [
-        ["minecraft:iron_ingot", 0.71],
         ["mana-and-artifice:vinteum_dust", 0.25],
-        ["thermal:ruby", 0.25],
         ["projectred-core:peridot", 0.29],
-        ["thermal:sapphire", 0.31],
         ["mekanism:fluorite_gem", 0.26],
-        ["mekanism:ingot_osmium", 0.25],
-        ["minecraft:lapis_lazuli", 0.30],
-        ["minecraft:redstone", 0.35],
+        ["minecraft:gold_ingot", 0.18],
+        ["2x minecraft:redstone", 0.23],
+        ["thermal:silver_ingot", 0.17],
+        ["thermal:nickel_ingot", 0.16],
     ]).fluid("minecraft:lava");
 });
