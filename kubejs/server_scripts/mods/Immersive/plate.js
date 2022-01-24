56206c68abd5e13841a04e5b60d060e0c7a1f78a
Gogo08 <gogo08190@gmail.com>
2022-01-24 09:01:09 +0100

    Plates recipe

--- kubejs/server_scripts/mods/Immersive/plate.js
+++ kubejs/server_scripts/mods/Immersive/plate.js
@@ -5,4 +5,17 @@ onEvent("recipes", (event) => {
       "draconicevolution:draconium_ingot",
       "immersiveengineering:mold_plate"
   );
+
+  event.remove({id: 'immersiveengineering:crafting/plate_constantan_hammering'})
+  event.remove({id: 'immersiveengineering:crafting/plate_electrum_hammering'})
+  event.remove({id: 'immersiveengineering:crafting/plate_nickel_hammering'})
+  event.remove({id: 'immersiveengineering:crafting/plate_silver_hammering'})
+  event.remove({id: 'immersiveengineering:crafting/plate_lead_hammering'})
+  event.remove({id: 'immersiveengineering:crafting/plate_tin_hammering'})
+  event.remove({id: 'immersiveengineering:crafting/plate_copper_hammering'})
+  event.remove({id: 'immersiveengineering:crafting/plate_gold_hammering'})
+  event.remove({id: 'immersiveengineering:crafting/plate_iron_hammering'})
+  event.remove({id: 'immersiveengineering:crafting/plate_steel_hammering'})
+  event.remove({id: 'immersiveengineering:crafting/plate_uranium_hammering'})
+  event.remove({id: 'immersiveengineering:crafting/plate_aluminum_hammering'})
 });
--- kubejs/server_scripts/mods/thermal/plate.js
+++ kubejs/server_scripts/mods/thermal/plate.js
@@ -1,3 +1,5 @@
 onEvent("recipes", (event) => {
   event.recipes.thermal.press("chromasky:draconium_plate", ["draconicevolution:draconium_ingot"]);
+  event.recipes.thermal.press("immersiveengineering:plate_steel", ["mekanism:ingot_steel"]);
+  event.recipes.thermal.press("immersiveengineering:plate_aluminum", ["immersiveengineering:ingot_aluminum"]);
 });
