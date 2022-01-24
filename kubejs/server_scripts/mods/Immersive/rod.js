01350b8009b10ab7247e76bc3986a16a31392212
Gogo08 <gogo08190@gmail.com>
2022-01-24 08:52:01 +0100

    Rod recipe

---
+++ kubejs/server_scripts/mods/Immersive/rod.js
@@ -0,0 +1,13 @@
+onEvent("recipes", (event) => {
+  event.remove({id: 'immersiveengineering:crafting/stick_aluminum'})
+  event.remove({id: 'immersiveengineering:crafting/stick_steel'})
+  event.remove({id: 'immersiveengineering:crafting/stick_iron'})
+  event.remove({id: 'immersiveposts:has_gold_rod'})
+  event.remove({id: 'immersiveposts:has_electrum_rod'})
+  event.remove({id: 'immersiveposts:has_copper_rod'})
+  event.remove({id: 'immersiveposts:has_lead_rod'})
+  event.remove({id: 'immersiveposts:has_silver_rod'})
+  event.remove({id: 'immersiveposts:has_nickel_rod'})
+  event.remove({id: 'immersiveposts:has_constantan_rod'})
+  event.remove({id: 'immersiveposts:has_uranium_rod'})
+});
