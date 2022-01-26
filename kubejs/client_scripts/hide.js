778384ffd1ef359f7305733cd3b39e3b02858fe4
Gogo08 <gogo08190@gmail.com>
2022-01-26 15:03:29 +0100

    Fix

--- kubejs/client_scripts/hide.js
+++ kubejs/client_scripts/hide.js
@@ -202,7 +202,6 @@ events.listen('jei.hide.items', function (event) {
     'losttrinkets:book_o_enchanting',
     'overloaded:ray_gun',
     'buildersaddition:iron_rod',
-    'projectex:final_star',
     Item.of('animalcrops:magnemone', '{entity:"minecraft:strider"}'),
     Item.of('animalcrops:spores', '{entity:"minecraft:piglin"}'),
     Item.of('animalcrops:spores', '{entity:"minecraft:hoglin"}'),
