048f935d9150a5abc3900efb3d1737bea94858ee
Gogo08 <gogo08190@gmail.com>
2022-01-26 15:02:51 +0100

    JEI hide

--- kubejs/client_scripts/hide.js
+++ kubejs/client_scripts/hide.js
@@ -201,7 +201,8 @@ events.listen('jei.hide.items', function (event) {
     'losttrinkets:dragon_breath',
     'losttrinkets:book_o_enchanting',
     'overloaded:ray_gun',
-    'buildersaddition:iron_rod'
+    'buildersaddition:iron_rod',
+    'projectex:final_star',
     Item.of('animalcrops:magnemone', '{entity:"minecraft:strider"}'),
     Item.of('animalcrops:spores', '{entity:"minecraft:piglin"}'),
     Item.of('animalcrops:spores', '{entity:"minecraft:hoglin"}'),
