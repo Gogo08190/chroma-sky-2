31cfafa9009c0fc2ae0d0032935847a1d5505e3c
Gogo08 <gogo08190@gmail.com>
2022-01-31 09:00:27 +0100

    Add warning on golden / netherite knife

--- kubejs/client_scripts/tools.js
+++ kubejs/client_scripts/tools.js
@@ -3,7 +3,7 @@ onEvent("item.tooltip", (event) => {
     event.add(/minecraft:(wooden|stone|iron|golden|diamond|netherite)_(sword|shovel|(pick)?axe|hoe)/, [
         Text.red("This item is for crafting only!"),
     ]);
-    
+
     event.add(/miniutilities:experience_pearl*/, [
         Text.red("Experience pearls are gotten when you kill a mob with experience harvester enchant."),
     ]);
@@ -13,4 +13,11 @@ onEvent("item.tooltip", (event) => {
     event.add("miniutilities:cursed_earth", [Text.green("Spawns hostile mobs")]);
     event.add("miniutilities:blessed_earth", [Text.green("Spawns passive mobs")]);
     event.add("miniutilities:blursed_earth", [Text.green("Spawns all mobs")]);
+
+    event.add('farmersdelight:netherite_knife', [
+        Text.red("Do not use on a Stuffed Hogin due to client crash."),
+    ]);
+    event.add('farmersdelight:golden_knife', [
+        Text.red("Do not use on a Stuffed Hogin due to client crash."),
+    ]);
 });
