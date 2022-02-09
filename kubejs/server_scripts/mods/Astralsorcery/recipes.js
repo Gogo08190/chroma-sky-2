ee7f49e2415e3ec51cea4699d5dd5408837a5626
Gogo08 <gogo08190@gmail.com>
2022-02-09 13:03:49 +0100

    Test nbt

--- kubejs/server_scripts/mods/Astralsorcery/recipes.js
+++ kubejs/server_scripts/mods/Astralsorcery/recipes.js
@@ -41,7 +41,39 @@ onEvent('recipes', event => {
         "inputFluid": "{FluidName:\"industrialforegoing:ether_gas\",Amount:1000}",
         "processingTime": 320,
         "output": {
-            "item": ('astralsorcery:rock_collector_crystal', '{astralsorcery:{constellation:"astralsorcery:aevitas",crystalProperties:{attributes:[{property:"astralsorcery:size",pLevel:3,discovered:1b},{property:"astralsorcery:purity",pLevel:2,discovered:1b},{property:"astralsorcery:shape",pLevel:3,discovered:1b},{property:"astralsorcery:collector.rate",pLevel:3,discovered:1b},{property:"astralsorcery:constellation.aevitas",pLevel:2,discovered:1b}]}}}')
+          "item": "astralsorcery:rock_collector_crystal",
+          "nbt": {
+            "astralsorcery":{
+              "constellation":"astralsorcery:aevitas",
+              "crystalProperties":{
+                "attributes":[{
+                  "property":"astralsorcery:size",
+                  "pLevel":3,
+                  "discovered":1b
+                },
+                {
+                  "property":"astralsorcery:purity",
+                  "pLevel":2,
+                  "discovered":1b
+                },
+                {
+                  "property":"astralsorcery:shape",
+                  "pLevel":3,
+                  "discovered":1b
+                },
+                {
+                  "property":"astralsorcery:collector.rate",
+                  "pLevel":3,
+                  "discovered":1b
+                },
+                {
+                  "property":"astralsorcery:constellation.aevitas",
+                  "pLevel":2,
+                  "discovered":1b
+                }
+              ]}
+            }
+          }
         },
         "type": "industrialforegoing:dissolution_chamber"
     }
