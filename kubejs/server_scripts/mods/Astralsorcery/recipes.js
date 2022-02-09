55f5fa0165c4d54313fd4641807c579f048485a0
Gogo08 <gogo08190@gmail.com>
2022-02-09 13:30:10 +0100

    Fix

--- kubejs/server_scripts/mods/Astralsorcery/recipes.js
+++ kubejs/server_scripts/mods/Astralsorcery/recipes.js
@@ -9,73 +9,48 @@ onEvent('recipes', event => {
     L: '#forge:dyes/white',
     I: `minecraft:stone`
   })
+});
 
-  event.custom(
-    {
-        "input": [
-            {
-                "item": "astralsorcery:rock_crystal"
-            },
-            {
-                "item": "astralsorcery:rock_crystal"
-            },
-            {
-                "item": "astralsorcery:rock_crystal"
-            },
-            {
-                "item": "astralsorcery:rock_crystal"
-            },
-            {
-                "item": "astralsorcery:rock_crystal"
-            },
-            {
-                "item": "astralsorcery:rock_crystal"
-            },
-            {
-                "item": "astralsorcery:rock_crystal"
-            },
-            {
-                "item": "astralsorcery:rock_crystal"
-            }
-        ],
-        "inputFluid": "{FluidName:\"industrialforegoing:ether_gas\",Amount:1000}",
-        "processingTime": 320,
-        "output": {
-          "item": "astralsorcery:rock_collector_crystal",
-          "nbt": {
-            "astralsorcery":{
-              "constellation":"astralsorcery:aevitas",
-              "crystalProperties":{
-                "attributes":[{
-                  "property":"astralsorcery:size",
-                  "pLevel":3,
-                  "discovered":1
-                },
-                {
-                  "property":"astralsorcery:purity",
-                  "pLevel":2,
-                  "discovered":1
-                },
-                {
-                  "property":"astralsorcery:shape",
-                  "pLevel":3,
-                  "discovered":1
-                },
-                {
-                  "property":"astralsorcery:collector.rate",
-                  "pLevel":3,
-                  "discovered":1
-                },
-                {
-                  "property":"astralsorcery:constellation.aevitas",
-                  "pLevel":2,
-                  "discovered":1
-                }
-              ]}
-            }
-          }
-        },
-        "type": "industrialforegoing:dissolution_chamber"
-    }
-  )
+onEvent('recipes', (event) => {
+    const recipes = [
+        {
+            inputs: [
+                'astralsorcery:rock_crystal',
+                'astralsorcery:rock_crystal',
+                'astralsorcery:rock_crystal',
+                'astralsorcery:rock_crystal',
+                'astralsorcery:rock_crystal',
+                'astralsorcery:rock_crystal',
+                'astralsorcery:rock_crystal',
+                'astralsorcery:rock_crystal'
+            ],
+            inputFluid: 'industrialforegoing:ether_gas',
+            inputFluidAmount: 1000,
+            processingTime: 600,
+            outputItem: Item.of('astralsorcery:rock_collector_crystal', '{astralsorcery:{constellation:"astralsorcery:aevitas",crystalProperties:{attributes:[{property:"astralsorcery:size",pLevel:3,discovered:1b},{property:"astralsorcery:purity",pLevel:2,discovered:1b},{property:"astralsorcery:shape",pLevel:3,discovered:1b},{property:"astralsorcery:collector.rate",pLevel:3,discovered:1b},{property:"astralsorcery:constellation.aevitas",pLevel:2,discovered:1b}]}}}').toJson(),
+            outputFluid: '',
+            outputFluidAmount: 0
+        }
+    ];
+
+    recipes.forEach((recipe) => {
+        let ingredients = [];
+
+        recipe.inputs.forEach((input) => {
+            ingredients.push(Ingredient.of(input));
+        });
+
+        const re = event.custom({
+            type: 'industrialforegoing:dissolution_chamber',
+            input: ingredients,
+            inputFluid: `{FluidName:"${recipe.inputFluid}",Amount:${recipe.inputFluidAmount}}`,
+            processingTime: recipe.processingTime,
+            output: recipe.outputItem,
+            outputFluid: `{FluidName:"${recipe.outputFluid}",Amount:${recipe.outputFluidAmount}}`
+        });
+
+        if (recipe.id) {
+            re.id(recipe.id);
+        }
+    });
 });
