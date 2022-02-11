4546018abc33e838dcdcb77d9db1571afdf36808
Gogo08 <gogo08190@gmail.com>
2022-02-11 19:54:58 +0100

    Pr Tools

--- kubejs/startup_scripts/vanillatools.js
+++ kubejs/startup_scripts/vanillatools.js
@@ -1,7 +1,22 @@
 const vanillaTools = Ingredient.of(/minecraft:(wooden|stone|iron|golden|diamond|netherite)_(sword|shovel|(pick)?axe|hoe)/);
+const prTools = Ingredient.of(/projectred-exploration:(ruby|peridot|sapphire)_(sickle|sword|(pick)?axe|hoe|shovel)/);
 
 onEvent("item.modification", (event) => {
     event.modify(vanillaTools, (tool) => {
+        tool.setMaxDamage(1);
+
+        tool.setTier((tier) => {
+            tier.uses = 1;
+            tier.speed = 0;
+            tier.attackDamageBonus = 0;
+            tier.level = 0;
+            tier.enchantmentValue = 0;
+        });
+    });
+});
+
+onEvent("item.modification", (event) => {
+    event.modify(prTools, (tool) => {
         tool.setMaxDamage(1);
 
         tool.setTier((tier) => {
