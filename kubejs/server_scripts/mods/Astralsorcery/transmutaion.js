d9cfda573ee185ad50f1cdcfefda1f772206b205
koepss <pierrelode2@outlook.fr>
2022-02-14 20:26:00 +0100

    add abyss melon recipe

--- kubejs/server_scripts/mods/Astralsorcery/transmutaion.js
+++ kubejs/server_scripts/mods/Astralsorcery/transmutaion.js
@@ -22,4 +22,28 @@ onEvent('recipes', event => {
       "starlight": 60
     }
   )
+
+  event.custom(
+    {
+      "type": "astralsorcery:block_transmutation",
+      "input": [
+        {
+          "block": "minecraft:melon",
+          "display": {
+            "item": "minecraft:melon",
+            "count": 1
+          }
+        }
+      ],
+      "output": {
+        "block": "theabyss:blue_melon_block"
+      },
+      "display": {
+        "item": "theabyss:blue_melon_block",
+        "count": 1
+      },
+      "starlight": 200
+    }
+  )
+
 });
