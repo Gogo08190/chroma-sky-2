6895801576c1b12a02c0ba843bd0141f238dafa5
koepss <pierrelode2@outlook.fr>
2022-02-14 14:25:32 +0100

    add pam's recipe

---
+++ kubejs/server_scripts/mods/pam's/recipes.js
@@ -0,0 +1,14 @@
+onEvent("recipes", (event) => {
+
+  // Leafy Fish Sandwich
+
+  event.shaped('pamhc2foodextended:leafyfishsandwichitem', [
+  'CP ',
+  'F  ',
+  '   '
+  ], {
+    F: '#forge:fish',
+    P: 'pamhc2foodcore:basicchickensandwichitem',
+    C: 'pamhc2foodcore:cuttingboarditem'
+  })
+});
