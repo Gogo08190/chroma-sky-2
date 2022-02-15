098ba0a11881888fe76c7d1aded353cc0935d744
Gogo08 <gogo08190@gmail.com>
2022-02-15 20:27:14 +0100

    Fix

--- kubejs/server_scripts/mods/pam's/recipes.js
+++ kubejs/server_scripts/mods/pam's/recipes.js
@@ -1,55 +1,16 @@
 onEvent("recipes", (event) => {
 
   // Leafy Fish Sandwich
-
-  event.shaped('pamhc2foodextended:leafyfishsandwichitem', [
-  'CP ',
-  'F  ',
-  '   '
-  ], {
-    F: '#forge:fish',
-    P: 'pamhc2foodcore:basicchickensandwichitem',
-    C: 'pamhc2foodcore:cuttingboarditem'
-  })
+  event.shapeless('pamhc2foodextended:leafyfishsandwichitem', ['#forge:fish', 'pamhc2foodcore:basicchickensandwichitem', 'pamhc2foodcore:cuttingboarditem'])
 
   // Raw Tofish
-
-  event.shaped('pamhc2foodextended:rawtofishitem', [
-  'MT ',
-  'CF ',
-  '   '
-  ], {
-    F: '#forge:fish',
-    M: 'pamhc2foodcore:mixingbowlitem',
-    C: 'pamhc2foodextended:soysauceitem',
-    T: 'pamhc2foodextended:firmtofuitem'
-  })
+  event.shapeless('pamhc2foodextended:rawtofishitem', ['#forge:fish', 'pamhc2foodcore:mixingbowlitem', 'pamhc2foodextended:soysauceitem', 'pamhc2foodextended:firmtofuitem'])
 
   // Peache and Cream Oatmeal
-
-  event.shaped('pamhc2foodextended:peachesandcreamoatmealitem', [
-  'PO ',
-  'AW ',
-  '   '
-  ], {
-    P: 'pamhc2foodcore:potitem',
-    O: 'pamhc2crops:oatsitem',
-    W: '#forge:water',
-    A: 'pamhc2trees:peachitem'
-  })
+  event.shapeless('pamhc2foodextended:peachesandcreamoatmealitem', ['pamhc2foodcore:potitem', 'pamhc2crops:oatsitem', '#forge:water', 'pamhc2trees:peachitem'])
 
   // Gourmet Mutton Patty
+  event.shapeless('pamhc2foodextended:gourmetmuttonpattyitem', ['pamhc2foodcore:mixingbowlitem', 'pamhc2foodcore:groundmuttonitem', 'pamhc2foodextended:saltandpepperitem', 'pamhc2crops:spiceleafitem', 'pamhc2crops:mustardseedsitem'])
 
-  event.shaped('pamhc2foodextended:gourmetmuttonpattyitem', [
-  'BGS',
-  'LM ',
-  '   '
-  ], {
-    B: 'pamhc2foodcore:mixingbowlitem',
-    G: 'pamhc2foodcore:groundmuttonitem',
-    S: 'pamhc2foodextended:saltandpepperitem',
-    L: 'pamhc2crops:spiceleafitem',
-    M: 'pamhc2crops:mustardseedsitem'
-  })
-
+  event.shapeless('pamhc2foodextended:gourmetmuttonpattyitem', ['pamhc2foodcore:saucepanitem', 'minecraft:porkchop', 'minecraft:honey_bottle', '#forge:butter', 'pamhc2trees:lemonitem'])
 });
