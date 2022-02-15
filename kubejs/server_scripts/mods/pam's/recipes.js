f5f5def55850d61d6ab4c3643e6546926445d179
koepss <pierrelode2@outlook.fr>
2022-02-15 16:05:47 +0100

    add craft pam's

--- kubejs/server_scripts/mods/pam's/recipes.js
+++ kubejs/server_scripts/mods/pam's/recipes.js
@@ -11,4 +11,45 @@ onEvent("recipes", (event) => {
     P: 'pamhc2foodcore:basicchickensandwichitem',
     C: 'pamhc2foodcore:cuttingboarditem'
   })
+
+  // Raw Tofish
+
+  event.shaped('pamhc2foodextended:rawtofishitem', [
+  'MT ',
+  'CF ',
+  '   '
+  ], {
+    F: '#forge:fish',
+    M: 'pamhc2foodcore:mixingbowlitem',
+    C: 'pamhc2foodextended:soysauceitem',
+    T: 'pamhc2foodextended:firmtofuitem'
+  })
+
+  // Peache and Cream Oatmeal
+
+  event.shaped('pamhc2foodextended:peachesandcreamoatmealitem', [
+  'PO ',
+  'AW',
+  '   '
+  ], {
+    P: 'pamhc2foodcore:potitem',
+    O: 'pamhc2crops:oatsitem',
+    W: '#forge:water',
+    A: 'pamhc2trees:peachitem'
+  })
+
+  // Gourmet Mutton Patty
+
+  event.shaped('pamhc2foodextended:peachesandcreamoatmealitem', [
+  'BGS',
+  'LM ',
+  '   '
+  ], {
+    B: 'pamhc2foodcore:mixingbowlitem',
+    G: 'pamhc2foodcore:groundmuttonitem',
+    S: 'pamhc2foodextended:saltandpepperitem',
+    L: 'pamhc2crops:spiceleafitem',
+    M: 'pamhc2crops:mustardseedsitem'
+  })
+
 });
