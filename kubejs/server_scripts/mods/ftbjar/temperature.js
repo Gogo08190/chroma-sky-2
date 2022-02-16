097a7e70a126cee6d3d857d1ea0b2dcc7318b629
Gogo08 <gogo08190@gmail.com>
2022-02-16 18:12:11 +0100

    Add temp

--- kubejs/server_scripts/mods/ftbjar/temperature.js
+++ kubejs/server_scripts/mods/ftbjar/temperature.js
@@ -14,9 +14,14 @@ onEvent("recipes", (event) => {
     };
 
     //Sub temp
-    source("subzero", "minecraft:snow_block", 0.25);
+    source("subzero", "minecraft:water", 0.20);
+    source("subzero", "minecraft:snow_block", 0.45);
     source("subzero", "minecraft:ice", 0.75);
     source("subzero", "minecraft:packed_ice", 2.0);
+    source("subzero", "kubejs:cryotheum", 3.0);
     source("subzero", "minecraft:blue_ice", 4.0);
     source("subzero", "powah:dry_ice", 6.0);
+
+    //Low Temp
+    source("low", "tconstruct:blazing_blood", 6.0);
 });
