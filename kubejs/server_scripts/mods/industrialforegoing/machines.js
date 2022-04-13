500f7851c8294d965424e452c55f9c7ebb77ca67
Gogo08 <gogo08190@gmail.com>
2022-04-13 13:37:06 +0200

    Nbt recipes Mystical factory

--- kubejs/server_scripts/mods/industrialforegoing/machines.js
+++ kubejs/server_scripts/mods/industrialforegoing/machines.js
@@ -89,10 +89,10 @@ events.listen('recipes', event => {
                 "item": "mysticalagriculture:inferium_gemstone_block"
             },
             {
-                "item": "industrialforegoing:processing_addon_2"
+                "item": Item.of('industrialforegoing:processing_addon_2', '{TitaniumAugment:{Processing:3.0f}}')
             },
             {
-                "item": "industrialforegoing:processing_addon_2"
+                "item": Item.of('industrialforegoing:processing_addon_2', '{TitaniumAugment:{Processing:3.0f}}')
             },
             {
                 "item": "mysticalagriculture:inferium_gemstone_block"
