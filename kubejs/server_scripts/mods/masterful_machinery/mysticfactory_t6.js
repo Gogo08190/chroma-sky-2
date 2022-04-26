b65cf0f8387b7de2a9147b2a5b0452874fa79f48
Gogo08 <41335079+Gogo08190@users.noreply.github.com>
2022-04-26 09:01:44 +0200

    Update mysticfactory_t6.js

--- kubejs/server_scripts/mods/masterful_machinery/mysticfactory_t6.js
+++ kubejs/server_scripts/mods/masterful_machinery/mysticfactory_t6.js
@@ -121,7 +121,7 @@ e.custom({
   mystic(1750, 'mysticalagriculture:aluminum_essence', 8, 'immersiveengineering:ingot_aluminum', 6)
   mystic(1750, 'mysticalagriculture:lumium_essence', 8, 'thermal:lumium_ingot', 6)
   mystic(1750, 'mysticalagriculture:emerald_essence', 8, 'minecraft:emerald', 6)
-  mystic(1750, 'mysticalagriculture:awakened_draconium_essence', 8, 'draconicevolution:awakened_draconium_nugget', 6)
+  mystic(1750, 'mysticalagriculture:awakened_draconium_essence', 9, 'draconicevolution:awakened_draconium_nugget', 6)
   mystic(1750, 'mysticalagriculture:spirited_crystal_essence', 9, 'powah:crystal_spirited', 6)
   mystic(1750, 'mysticalagriculture:niotic_crystal_essence', 9, 'powah:crystal_niotic', 6)
   mystic(1750, 'mysticalagriculture:nitro_crystal_essence', 9, 'powah:crystal_nitro', 6)
